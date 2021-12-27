37081694ce07e1cd96061bef1f522164bcb943f8
Bruna Gonçalves <bruna.goncalves@luizalabs.com>
2021-12-27 16:21:43 -0300

    adiciona requests

---
+++ src/main/java/unifacef/clientapi/gateways/inputs/http/requests/ClientRequest.java
@@ -0,0 +1,36 @@
+package unifacef.clientapi.gateways.inputs.http.requests;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import unifacef.clientapi.domains.Address;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Data
+public abstract class ClientRequest implements Serializable {
+
+    private static final long serialVersionUID = 3475925051912891191L;
+
+    @ApiModelProperty(position = 1)
+    @NotNull(message = "{required.field}")
+    private String name;
+
+    @ApiModelProperty(position = 2)
+    @NotNull(message = "{required.field}")
+    private String phone;
+
+    @ApiModelProperty(position = 3)
+    private String driverLicense;
+
+    @ApiModelProperty(position = 4)
+    private Boolean active;
+
+    @ApiModelProperty(position = 5)
+    @NotNull(message = "{required.field}")
+    private Address address;
+
+
+
+
+}
