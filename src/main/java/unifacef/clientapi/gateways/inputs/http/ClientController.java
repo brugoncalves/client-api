a474372798c81fad8494171e27ac04efd6d22b5d
Bruna Gonçalves <bruna.goncalves@luizalabs.com>
2021-12-27 16:19:53 -0300

    adiciona controllers

--- src/main/java/unifacef/clientapi/gateways/inputs/http/ClientController.java
+++ src/main/java/unifacef/clientapi/gateways/inputs/http/ClientController.java
@@ -6,12 +6,16 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import unifacef.clientapi.domains.Client;
+import unifacef.clientapi.gateways.inputs.http.requests.CreateClientRequest;
+import unifacef.clientapi.gateways.inputs.http.requests.UpdateClientRequest;
 import unifacef.clientapi.gateways.inputs.http.responses.ClientResponse;
 import unifacef.clientapi.gateways.inputs.http.responses.ListResponse;
+import unifacef.clientapi.usecases.CreateClient;
+import unifacef.clientapi.usecases.FindByClientId;
 import unifacef.clientapi.usecases.FindClients;
+import unifacef.clientapi.usecases.UpdateClient;
 
 @Slf4j
 @Validated
@@ -20,11 +24,35 @@ import unifacef.clientapi.usecases.FindClients;
 @RequestMapping(value = "/api/v1/clients")
 public class ClientController {
 
+
+    private final CreateClient createClient;
+    private final UpdateClient updateClient;
+    private final FindByClientId findByClientId;
     private final FindClients findClients;
 
+    @GetMapping
     public ListResponse<ClientResponse> findByPage(@RequestParam(defaultValue = "0") final Integer page,
                                                    @RequestParam(defaultValue = "20") final Integer size){
         Page<ClientResponse> clientPage = findClients.execute(PageRequest.of(page, size)).map(ClientResponse::new);
         return new ListResponse<>(clientPage);
+    }
+
+    @PostMapping
+    public ClientResponse create(@RequestBody @Validated final CreateClientRequest request) {
+        Client client = createClient.execute(request.toDomain());
+        return new ClientResponse(client);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ClientResponse update(@PathVariable final String id,
+                                 @RequestBody @Validated final UpdateClientRequest request) {
+        Client client = updateClient.execute(request.toDomain(id));
+        return new ClientResponse(client);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ClientResponse find(@PathVariable final String id) {
+        Client client = findByClientId.execute(id);
+        return new ClientResponse(client);
     }
 }
